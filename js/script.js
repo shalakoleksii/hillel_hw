0be36cba050e6e9177d8822d7548eec0cf35b24a
Oleksii Shalak <shalak.alexey@icloud.com>
2025-11-19 14:49:25 +0200

    hw_2 complited

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const userName = prompt('Enter your name', '');
+
+if (userName) {
+    const showGreeting = confirm(`Hi ${userName}!, Do you want to see a greeting?`);
+
+    if (showGreeting) {
+        alert(`Hello, ${userName}! How are you?`);
+    }
+}
