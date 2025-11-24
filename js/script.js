e7339523861dc8c3916863ac124323800e2e73bc
Oleksii Shalak <shalak.alexey@icloud.com>
2025-11-24 13:24:47 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const priceInput = prompt('Введіть початкову ціну товару');
+const price = Number(priceInput);
+
+if(isNaN(price) || price <= 0) {
+  alert('Помилка вводу')
+} else {
+  const discountInput = prompt('Введіть розмір знижки у відсотках');
+  const discount = Number(discountInput);
+
+  if(isNaN(discount) || discount <= 0) {
+    alert('Помилка вводу')
+  } else {
+    const discountedPrice = price - (price * discount / 100);
+    alert(`Початкова ціна: ${price} грн, знижка: ${discount}%. Ціна зі знижкою: ${discountedPrice} грн`)
+  }
+}
