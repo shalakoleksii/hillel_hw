5c384ec5c4fe464185f02231676672d7023c8701
Oleksii Shalak <shalak.alexey@icloud.com>
2025-11-24 15:24:24 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const inputAge = prompt('Введить Ваш вік');
+
+if(inputAge === null || inputAge === "") {
+  alert('Вік не вказано');
+} else {
+  const age = Number(inputAge);
+
+  if(age === null || age <= 0) {
+    alert('Вік не коректний');
+  } else if (age < 18) {
+    const withAdult = confirm('Вам менше 18. Чи є з вами дорослий, який дозволяє перегляд?');
+
+    if(withAdult) {
+      alert('Доступ дозволено з дозволу дорослого.');
+    } else {
+      alert('Доступ заборонено.');
+    }
+  } else {
+    alert('Доступ дозволено. Приємного перегляду!');
+  }
+}
